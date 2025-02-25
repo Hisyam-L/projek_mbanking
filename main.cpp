2bcf3b821d928feabee70ca271e36cf5b6c08a1f
Hisyam-L <hisyamlbaihaqi01@gmail.com>
2025-02-25 13:21:56 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,137 @@
+#include "function.h"
+int jml_user = 0;
+
+int main(){
+
+    bool ulang = true,pilihan= true, loginulang = true;
+    int x;
+    
+    do
+    {
+        int pil ;
+        system("cls");
+        cout << "Selamat datang di bank CIHUY";
+        cout << "\n1. Daftar Akun Baru";
+        cout << "\n2. Login Akun";
+        cout << "\n3. Keluar";
+        cout << "\n\nPilih menu : ";
+        cin >> pil;
+        cin.ignore();
+
+        switch (pil)
+        {
+        case 1 : do
+        {
+            system("cls");
+            cout << "Masukkan nama          : ";
+            getline(cin, akunarray[jml_user].nama);
+            cout << "Masukkan sandi         : ";
+            getline(cin, akunarray[jml_user].sandi);
+            cout << "Masukkan alamat        : ";
+            getline(cin, akunarray[jml_user].alamat);
+            cout << "Masukkan pekerjaan     : ";
+            getline(cin, akunarray[jml_user].pekerjaan);
+            cout << "Masukkan nomor telepon : ";
+            getline(cin, akunarray[jml_user].no_telp);
+            cout << "Masukkan NIK           : ";
+            cin >> akunarray[jml_user].NIK;
+            cin.ignore(); 
+        
+            system("cls");
+            cout << "\nNama         : " << akunarray[jml_user].nama ;
+            cout << "\nSandi        : " << akunarray[jml_user].sandi ;
+            cout << "\nAlamat       : " << akunarray[jml_user].alamat ;
+            cout << "\nPekerjaan    : " << akunarray[jml_user].pekerjaan ;
+            cout << "\nNomor Telepon: " << akunarray[jml_user].no_telp;
+            cout << "\nNIK          : " << akunarray[jml_user].NIK <<endl;
+            // cin.get();
+            do
+            {
+                cout << "Apakah Data Anda Sudah Benar\n1). Benar\n2). Salah\n"; cin >> x;
+                if(x == 1){
+                    jml_user++;
+                    pilihan = false;
+                }
+                else if(x == 2){
+                    pilihan = true;
+                }
+            } while (x != 1 && x != 2);
+
+        } while (pilihan == true);
+        
+            break;
+        case 2 :
+        do
+        {
+            system("cls");
+            bool akun_ditemukan = false;
+            if (jml_user == 0)
+            {
+                cout << "Anda belum memiliki akun, buat terlebih dahulu";
+                cin.get();
+                loginulang = false;
+                break;
+
+            }
+            
+            Login userlogin;
+            int pilihanlogin;
+            system("cls");
+            cout << "Masukkan nama          : ";
+            getline(cin, userlogin.namaLogin);
+            cout << "Masukkan sandi         : ";
+            getline(cin, userlogin.sandiLogin);
+
+            system("cls");
+            for (int i = 0; i < jml_user; i++)
+            {
+                if (userlogin.namaLogin == akunarray[i].nama && userlogin.sandiLogin == akunarray[i].sandi)
+                {
+                    akun_ditemukan = true;
+                    cout << "Selamat datang " << akunarray[i].nama << endl;
+                    cin.get();
+
+                    pilihanmember();
+                    loginulang = false;
+                    
+
+                }
+                
+            }
+            if (akun_ditemukan == false)
+            {
+                system("cls");
+                    cout <<"Sandi atau nama akun anda salah";
+                    cout<<"\nApakah ingin mengulangi lagi ?\n1. Iya\n2. Tidak\nMasukan pilihan anda : ";
+                    cin>>pilihanlogin;
+                    cin.ignore();
+                    if (pilihanlogin == 1)
+                    {
+                        loginulang = true;
+                    }
+                    else if (pilihanlogin == 2)
+                    {
+                        loginulang = false;
+                    }
+                    else{
+                        cout<<"Pilihan tidak ada";
+                    }
+                    
+            }
+            
+            
+        } while (loginulang == true );
+         
+        break;
+
+
+        case 3:
+        ulang = false;
+            break;
+        }
+
+
+    } while (ulang == true);
+
+    
+}
